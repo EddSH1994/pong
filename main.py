0a0181f5c3c41dc9dea47dd77b4f5841e0f9a94d
EddSH1994 <edd@schauman-haigh.co.uk>
2023-01-07 12:18:04 -0800

    Add movement, refactor:

--- main.py
+++ main.py
@@ -1,43 +1,85 @@
-from abc import ABC, abstractmethod
 import pygame
 import sys
 
+# constants
+# game
+GAME_CAPTION = 'Pong'
+
+# window
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 400
+WINDOW_TOP_LEFT = (0, 0)
+WINDOW_CENTER_X = (WINDOW_WIDTH / 2)
+WINDOW_CENTER_Y = (WINDOW_HEIGHT / 2)
+
+# colors
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+
+# game objects
+BALL_WIDTH = 10
+BALL_HEIGHT = 10
+PADDLE_WIDTH = 10
+PADDLE_HEIGHT = 50
+PLAYER_STARTING_X = 10
+PLAYER_STARTING_Y = (WINDOW_HEIGHT / 2 - PADDLE_HEIGHT / 2)
+OPPONENT_STARTING_X = (WINDOW_WIDTH - PADDLE_WIDTH - 10)
+OPPONENT_STARTING_Y = (WINDOW_HEIGHT / 2 - PADDLE_HEIGHT / 2)
+
 # set up pygame
 pygame.init()
+pygame.display.set_caption(GAME_CAPTION)
 
 # set up the window
-surface = pygame.display.set_mode((500, 400), 0, 32)
+window_size = (WINDOW_WIDTH, WINDOW_HEIGHT)
+screen = pygame.display.set_mode(window_size)
+background = pygame.Surface(window_size)
+screen.blit(background, WINDOW_TOP_LEFT)
+
 clock = pygame.time.Clock()
 
-pygame.display.set_caption('Pong')
 
-
-class GameObject(ABC):
-    def __init__(self, width, height, color):
+class GameObject:
+    def __init__(self, width, height, color, x, y):
         self.width = width
         self.height = height
         self.color = color
+        self.x = x
+        self.y = y
 
-    @abstractmethod
+    def initialize(self):
+        pygame.draw.rect(screen, color=self.color, rect=[self.x, self.y, self.width, self.height])
+
     def move(self, x, y):
-        pass
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        pygame.draw.rect(screen, color=self.color, rect=[self.x, self.y, self.width, self.height])
 
 
-class Player(GameObject):
-    def move(self, x, y):
-        pygame.draw.rect(surface, color=self.color, rect=[x, y, self.width, self.height])
-
-
-player = Player(10, 50, (0, 0, 0))
-opponent = Player(10, 50, (0, 0, 0))
+player = GameObject(PADDLE_WIDTH, PADDLE_HEIGHT, COLOR_BLACK, PLAYER_STARTING_X, PLAYER_STARTING_Y)
+opponent = GameObject(PADDLE_WIDTH, PADDLE_HEIGHT, COLOR_BLACK, OPPONENT_STARTING_X, OPPONENT_STARTING_Y)
+ball = GameObject(BALL_WIDTH, BALL_HEIGHT, COLOR_BLACK, WINDOW_CENTER_X, WINDOW_CENTER_Y)
 
 
 def start_game():
-    surface.fill((255, 255, 255))
+    screen.fill(COLOR_WHITE)
 
-    player.move(10, 175)
-    opponent.move(480, 175)
-    pygame.display.flip()
+    player.initialize()
+    opponent.initialize()
+    ball.initialize()
+
+
+def draw_screen():
+    screen.fill((255, 255, 255))
+    player.draw()
+    opponent.draw()
+    ball.draw()
+
+
+def update_screen():
+    pygame.display.update()
 
 
 if __name__ == '__main__':
@@ -50,5 +92,11 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    player.move(player.x, player.y - 10)
+                if event.key == pygame.K_DOWN:
+                    player.move(player.x, player.y + 10)
 
-        pygame.display.flip()
+        draw_screen()
+        update_screen()
